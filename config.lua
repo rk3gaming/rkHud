4e7f935684a1bfc5d4b3c83700399559409327ab
RK <rkfrmda3@gmail.com>
2025-06-03 18:41:28 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+return {
+    debug = false,
+    
+    framework = "esx", -- Supported options, esx, qb, qbox, ox, standalone.
+
+    commands = {
+        settings = "hud",
+        togglehud = "togglehud"
+    }
+}
